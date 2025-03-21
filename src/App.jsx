19a7e8df234c7959c13704e0c17a6b801e21de77
abhishekp-dizzaroo <abhishekp@dizzaroo.com>
2025-03-21 10:59:14 +0530

    project setup by using vite React js with shadcn ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl bg-black text-white text-center">NeuroDoc</h1>
+      <Button className="mt-1">Add Button</Button>
+    </div>
+  )
+}
+
+export default App
